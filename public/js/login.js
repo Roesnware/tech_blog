babeefd7373daf85e83aa3809766c9d4ac40bfc3
Roesn <Bubwub04@icloud.com>
2023-11-07 08:40:29 -0500

    comments

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,24 @@
+// func to handle login 
 const loginFormHandler = async (event) => {
+    // prevent default 
     event.preventDefault();
   
     // Collect values from the login form
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
+    // make sure values alll filled out  
     if (username && password) {
-      // Send a POST request to the API endpoint
+      // send a post 
       const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
+      // successful login
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
+        // redirect the browser to the dashboard page
         document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
@@ -22,11 +26,13 @@ const loginFormHandler = async (event) => {
     }
   };
 
+  // func to send to sign up page
   const sendToSignUp = () => {
     
     document.location.replace('/signup');
   };
   
+  // event listeneers
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler); 
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,25 @@
+// fun to handle posting info and signing up
 const signupFormHandler = async (event) => {
+    // prevent default
     event.preventDefault();
   
+    // hooks
     const name = document.querySelector('#name-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
+    // make sure values all fille dout 
     if (name && username && password) {
+      // post to signup 
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ name, username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
+      // successful post
       if (response.ok) {
+        // redirect to homepage 
         document.location.replace('/homepage');
       } else {
         alert(response.statusText);
@@ -20,11 +27,13 @@ const signupFormHandler = async (event) => {
     }
   };
 
+  // func to set page back to login
   const sendToLogIn = () => {
 
     document.location.replace('/login');
   }
 
+  // event listneers
   document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
