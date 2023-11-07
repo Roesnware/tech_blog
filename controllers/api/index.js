434bdaa0c118a5fe24850b1c149de0f6aa30923f
Roesn <Bubwub04@icloud.com>
2023-11-07 06:29:07 -0500

    typo

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 // import routes
 const userRoutes = require('./userRoutes.js');
-const reviewRoutes = require('./reviewRoutes.js/index.js');
+const reviewRoutes = require('./reviewRoutes.js');
 
 // use routes 
 router.use('/users', userRoutes);
