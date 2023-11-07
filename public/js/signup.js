74f832a7e358f23d91d0957e9748294f09e6572f
Roesn <Bubwub04@icloud.com>
2023-11-07 08:24:00 -0500

    fixed login/signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,18 +2,18 @@ const signupFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
+    if (name && username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/homepage');
       } else {
         alert(response.statusText);
       }
